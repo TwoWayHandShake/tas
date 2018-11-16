cc00a2c918acdab211505e976b43f2f639c12754
Daniel Orlowski <>
2018-11-16 12:22:27 +0100

    First animation

--- src/components/header.js
+++ src/components/header.js
@@ -2,26 +2,13 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
+  <div>
     <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
+    
     >
-      <h1 style={{ margin: 0 }}>
+      <h1>
         <Link
           to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
         >
           {siteTitle}
         </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,12 +6,20 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Daaaaaaniiiiels seiiibsdfte</h1>
-    <p>Ein Versuch ok. Ein Versuch.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+
+    <h1>Daaaaaaniiiiels efa</h1>
+    <div class="circleW1"></div>
+
+    <div class="circle1"></div>
+    <div class="circle2"></div>
+    
+    <div class="circleR1"></div>
+    <div class="circleR2"></div>
+  
+   <div class="circleY2"></div>
+   <p>Ein Versuch ok. Ein Versuch.</p>
+    <Link to="/page-2/">Zum Gästebuch</Link>
   </Layout>
 )
 
